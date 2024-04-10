8b65bb397291f42a36dded768bbe6576029096f6
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-04-10 19:03:08 -0300

    parte do layout está pronta

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,37 @@
+import { Container } from "./Styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>
+                            Desenvolvimento de website
+                        </td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/04/2024</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Desenvolvimento de website
+                        </td>
+                        <td className="withdrow">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/04/2024</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
