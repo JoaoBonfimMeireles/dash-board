b029e4ecb05c71f02bff82455f1f2c5706830065
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-04-11 23:58:35 -0300

    inicio da edição do modal

--- src/components/NewTransctionModal/NewTransctionModal.tsx
+++ src/components/NewTransctionModal/NewTransctionModal.tsx
@@ -1,17 +1,37 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
-interface NewTransctionModalProps{
+interface NewTransctionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
-export function NewTransctionModal({isOpen, onRequestClose}: NewTransctionModalProps) {
+export function NewTransctionModal({ isOpen, onRequestClose }: NewTransctionModalProps) {
     return (
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
         >
-            <h2>Cadastrar transação</h2>
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input
+                    placeholder='Titulo'
+                />
+
+                <input
+                    type='number'
+                    placeholder='Valor'
+                />
+
+                <input
+                    placeholder='Categoria'
+                />
+
+                <button type='submit'>
+                    cadastrar
+                </button>
+            </Container>
         </Modal>
     )
 };
