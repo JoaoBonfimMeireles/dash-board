2fb3abc701dc5635f3fe0d286d621dcb452bd22f
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-04-11 21:27:52 -0300

    adição do modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+import Modal from 'react-modal';
 import { Logo } from "../../assets/Logo";
 import { Container, Content } from "./styles";
 
 export function Header() {
+
+    const [ insNewTransactionalModalOpen, setInsNewTransactionalModalOpen ] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setInsNewTransactionalModalOpen(true);
+    }
+
+    function handleClosenNewTransactionModal() {
+        setInsNewTransactionalModalOpen(false);
+    }
+
     return (
         <Container>
             <Content>
                 <Logo />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova Transação
                 </button>
+
+                <Modal isOpen={insNewTransactionalModalOpen} onRequestClose={handleClosenNewTransactionModal}>
+                    <h2>Cadastrar transação</h2>
+                </Modal>
             </Content>
         </Container>
     )
